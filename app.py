23d6ce720f993a578a2eba115f115a1df5bab647
Anmol Gautam <anmol100gautam+github@gmail.com>
2025-05-02 17:12:15 +0530

    Update app.py
    added some more categories

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("🤖 IntelecView AI powered Interview Question Generator & Evaluator")
 
 st.markdown("Prepare for your interviews using AI!")
 
-category = st.selectbox("Select Interview Category", ["Data Structures", "System Design", "HR","UPSC"])
+category = st.selectbox("Select Interview Category", ["Data Structures", "System Design","Marketing","Sales","DBMS", "HR","UPSC"])
 difficulty = st.radio("Select Difficulty", ["Easy", "Medium", "Hard"])
 
 if st.button("🎯 Generate Question"):
